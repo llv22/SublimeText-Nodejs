a6b4e08d746a783b6f97d63fde861ba26c70f943
Orlando Ding <xiandao.airs@gmail.com>
2013-05-02 21:04:14 +0800

    llv22 - new changes for function.line of logging for debugging

--- sychfrompackages.sh
+++ sychfrompackages.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cp -rvf /Users/llv22/Library/Application\ Support/Sublime\ Text\ 2/Packages/Nodejs/* . 
+# see http://www.huzs.net/?p=920
+rsync -avr --progress /Users/llv22/Library/Application\ Support/Sublime\ Text\ 2/Packages/Nodejs/* . --exclude Readme.md
 rm *.pyc
 rm lib/*.pyc
